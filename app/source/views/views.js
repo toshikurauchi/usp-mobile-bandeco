b53c7afcbc3a3f4363e93fc5aec1de426150f2c0
Igor Montagner <igordsm@gmail.com>
2014-06-15 11:00:16 -0300

    Agora com seletor de restaurante!

--- app/source/views/views.js
+++ app/source/views/views.js
@@ -8,10 +8,22 @@ enyo.kind({
 	name: "usp.Menu",
 	kind: "enyo.Collection",
 	source: "jsonp",
-	url: "https://query.yahooapis.com/v1/public/yql?q=use%20'https%3A%2F%2Fraw.githubusercontent.com%2Ftoshikurauchi%2Fusp-mobile-bandeco%2Fmaster%2Fusp.bandejao.xml'%3B%0Aselect%20*%20from%20usp.bandejao%20where%20bandejao%3D%22pusp%22%3B&format=json&diagnostics=true",
+	url: "https://query.yahooapis.com/v1/public/yql?q=use%20'https%3A%2F%2Fraw.githubusercontent.com%2Ftoshikurauchi%2Fusp-mobile-bandeco%2Fmaster%2Fusp.bandejao.xml'%3B%0Aselect%20*%20from%20usp.bandejao%20where%20bandejao%3D%22central%22%3B&format=json",
+	
+	published: {
+		bandejao: "central",
+		displayName: "Central"
+	},
+	
+	bindings: [
+		{from: ".bandejao", to: ".url", transform: function(val) {
+			return "https://query.yahooapis.com/v1/public/yql?q=use%20'https%3A%2F%2Fraw.githubusercontent.com%2Ftoshikurauchi%2Fusp-mobile-bandeco%2Fmaster%2Fusp.bandejao.xml'%3B%0Aselect%20*%20from%20usp.bandejao%20where%20bandejao%3D%22" + val + "%22%3B&format=json";
+		}},
+	],	
+	
 	parse: function (data) {
 		return data.query.results.cardapio.refeicao;
-	}
+	},
 });
 
 enyo.kind({
@@ -63,62 +75,49 @@ enyo.kind({
 		menu: null,
 	},
 	
+	restaurants: {},
+	
 	components:[
 		{kind: "onyx.Toolbar",
 		 layoutKind: "FittableColumnsLayout",
 		 components: [
 			{content: "USP-Mobile - Cardápios", fit: true},
-			{kind: "onyx.Button", content: "Atualizar", ontap: "updateMenu"}, // trocar por icon button depois
+			{kind: "onyx.Button", content: "Atualizar", ontap: "updateMenus"}, // trocar por icon button depois
 		]},
-			/*{kind: "FittableColumns", components: [
-				{kind: "onyx.PickerDecorator", components: [
-					{},
-					{kind: "onyx.Picker", onSelect: "selectMenu", components: [
-								{content: "Central", active: true},
-								{content: "Física"},
-								{content: "Química"},
-								{content: "PCO"},
-							]},
-						]
-					},
-					{kind: "onyx.PickerDecorator", components: [
-						{},
-						{kind: "onyx.Picker", onSelect: "selectMenu", components: [
-								{content: "Segunda", active: true},
-								{content: "Terça"},
-								{content: "Quarta"},
-								{content: "Quinta"},
-								{content: "Sexta"},
-								{content: "Sábado"},
-								{content: "Domingo"},
-							]},
-						]
-					},
-					{kind: "onyx.PickerDecorator", onSelect: "selectMenu", components: [
-						{},
-						{kind: "onyx.Picker", components: [
-								{content: "Almoço", active: true},
-								{content: "Jantar"},
-							]},
-						]
-					},
-			]},*/
-			{kind: "enyo.FittableColumns", fit: true, components: [
-				{kind: "onyx.Button", content: "<", ontap: "prevDay"},
-				{kind: "enyo.Panels", name: "mealsPanel", fit: true, components: [
-					{name: "emptyMenu", content: "Nenhum cardápio encontrado!."},
-				]},
-				{kind: "onyx.Button", content: ">", ontap: "nextDay"},
+		{kind: "onyx.PickerDecorator", components: [
+			{style: "min-width: 150px"},
+			{kind: "onyx.Picker", onSelect: "selectRestaurant", components: [
+				{content: "Central", active: true},
+				{content: "Física"},
+				{content: "Química"},
+				{content: "Prefeitura"},
+				{content: "Doc?"},
+				{content: "Enfermagem"},
+				{content: "FSP"},
+				{content: "Direito"},
 			]},
+		]},
+		{kind: "enyo.FittableColumns", fit: true, components: [
+			{kind: "onyx.Button", content: "<", ontap: "prevDay"},
+			{kind: "enyo.Panels", name: "mealsPanel", fit: true, components: [
+				{name: "emptyMenu", content: "Nenhum cardápio encontrado!."},
+			]},
+			{kind: "onyx.Button", content: ">", ontap: "nextDay"},
+		]},
 	],
 	
 	create: function () {
 		this.inherited(arguments);
-		this.updateMenu();
+		//this.updateMenu();
 		var i;
 		for (i = 0; i < 12; i++) {
 			this.$.mealsPanel.createComponent({kind: 'usp.MenuDisplayDay'});
 		}
+		var t = this;
+		this.updateMenus({success: function (m) {
+			t.set('menu', t.restaurants["Central"]);
+		}});
+		
 	},
 	
 	nextDay: function (is, ievt) {
@@ -140,24 +139,46 @@ enyo.kind({
 		}
 	},
 	
-	updateMenu: function(inSender, inEvent) {
-		// show spinner
+	updateMenus: function (opts) {
+		var restaurantes = {"Central": "central",
+							"Física": "fisica",
+							"Química": "quimica",
+							"Prefeitura": "pusp",
+							"Doc?": "doc",
+							"Enfermagem": "enf",
+							"FSP": "fsp",
+							"Direito": "direito"};
+		var keys = Object.keys(restaurantes);
+		var loaded = 0;
 		var t = this;
-		var menus = new usp.Menu();
-		menus.fetch({
-			success: function (d) {
-				t.set('menu', d);
-				var today = new Date();
-				var dw = today.getDay();
-				var index = (dw - 1 + 7) % 7;
-				if (index > 5) {
-					index += 10;
-				} else {
-					index = index * 2;
+		for (i = 0; i < keys.length; i++) {
+			var menu = new usp.Menu();
+			menu.set('bandejao', restaurantes[keys[i]]);
+			menu.set('displayName', keys[i]);
+			
+			menu.fetch({success: function (m) {
+				t.restaurants[m.get('displayName')] = m;
+				loaded++;
+				if (loaded == keys.length) {
+					var today = new Date();
+					var dw = today.getDay();
+					var index = (dw - 1 + 7) % 7;
+					if (index > 5) {
+						index += 10;
+					} else {
+						index = index * 2;
+					}
+					index++;
+					t.$.mealsPanel.setIndex(index);
+					if (opts.success !== undefined) {
+						opts.success(m);
+					}
 				}
-				index++;
-				t.$.mealsPanel.setIndex(index);
-			}
-		});
+			}});
+		}
+	},
+	
+	selectRestaurant: function (is, ievt) {
+		this.set('menu', this.restaurants[is.get('selected').content]);
 	},
 });
